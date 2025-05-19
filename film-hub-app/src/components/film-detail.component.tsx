d53a2524c4dc48c6241e48e495c3647c05ce9ec8
Takács Dániel <takacsdaniel.inf@gmail.com>
2025-05-19 23:55:27 +0200

    listen films style

--- film-hub-app/src/components/film-detail.component.tsx
+++ film-hub-app/src/components/film-detail.component.tsx
@@ -1,13 +1,22 @@
 const FilmDetail = (props: any) => {
   const { film } = props;
   return (
-    <div className="flex flex-col bg-black/40">
+    <div className="flex flex-col">
       <div>
-        <img src={`https://image.tmdb.org/t/p/w154${film.poster_path}`} />
+        <img
+          className="rounded-tl-xs rounded-tr-xs "
+          src={`https://image.tmdb.org/t/p/w185${film.poster_path}`}
+        />
       </div>
-      <div>
-        <div className="font-title">{film.title}</div>
-        <span>{film.vote_average.toFixed(1)} / 10</span>
+      <div className="p-2 bg-white/40 rounded-br-sm rounded-bl-sm h-20 flex flex-col justify-between">
+        <div className="font-title text-md max-w-40">{film.title}</div>
+        <div className="flex justify-between text-white/50 text-xs italic">
+          <span>
+            {film.vote_average.toFixed(1)} / 10 ({film.vote_count})
+          </span>
+          <span></span>
+          <span>{film.release_date.substring(0, 4)}</span>
+        </div>
       </div>
     </div>
   );
--- film-hub-app/src/components/film-list.component.tsx
+++ film-hub-app/src/components/film-list.component.tsx
@@ -6,11 +6,11 @@ const FilmList = (props: any) => {
   const { films = [], isLoading, error } = useFilmFetch();
   return (
     <div className="w-3/4 p-4">
-      <h2 className="border-b">{listName}</h2>
+      <h2 className="border-b mb-5">{listName}</h2>
       {error && <div>404 ERROR</div>}
       {isLoading && <div>Loading...</div>}
       {films && (
-        <div className="grid grid-cols-5 w-max gap-4">
+        <div className="grid grid-cols-5 w-max gap-10">
           {films.map((film: any) => {
             return <FilmDetail key={film.id} film={film} />;
           })}
