0795c016c0cba574cf2abce8a40bf369ecc23d80
Takács Dániel <takacsdaniel.inf@gmail.com>
2025-05-21 22:35:02 +0200

    film ranking cards on diff view, added ranking placement

--- film-hub-app/src/components/film-list.component.tsx
+++ film-hub-app/src/components/film-list.component.tsx
@@ -2,28 +2,51 @@ import { useEffect, useState } from "react";
 import useFilmFetch from "../hooks/useFilmFetch";
 import FilmDetail from "./film-detail.component";
 import { TfiAngleRight } from "react-icons/tfi";
+import { TfiAngleLeft } from "react-icons/tfi";
 
 const FilmList = (props: any) => {
   const { listName } = props;
   const { films = [], isLoading, error } = useFilmFetch();
   const [visibleFilms, setVisibleFilms] = useState([]);
   const [visibleCount, setVisibleCount] = useState(5);
+  const [onFirstSet, setOnFirstSet] = useState(true);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (onFirstSet && films) setVisibleFilms(films.slice(0, 5));
+    else if (films) setVisibleFilms(films.slice(5, 10));
+  }, [films, onFirstSet]);
+
+  const handleOnNextFilmsClick = () => {
+    setOnFirstSet(!onFirstSet);
+  };
 
   return (
-    <div className=" p-4">
-      <h2 className="border-b mb-5">{listName}</h2>
-      <div className="flex justify-between items-center">
+    <div className=" p-4 w-max">
+      <h2 className="border-b mb-5 p-2 text-2xl">{listName}</h2>
+      <div className="flex justify-between items-center px-8">
         {error && <div>404 ERROR</div>}
         {isLoading && <div>Loading...</div>}
-        <div className="flex flex-wrap w-max gap-10">
-          {films?.map((film: any) => {
-            return <FilmDetail key={film.id} film={film} />;
+        <div className="flex flex-wrap gap-10">
+          {visibleFilms?.map((film: any, index: number) => {
+            return (
+              <div key={index} className="relative">
+                <span className="absolute -left-5 font-bold text-3xl font-ranking">
+                  {index + 1}
+                </span>
+                <FilmDetail key={film.id} film={film} />
+              </div>
+            );
           })}
         </div>
-        <button className="cursor-pointer bg-white/20 h-max w-max py-6 rounded-xs hover:bg-white/30 duration-75 hover:scale-105">
-          <TfiAngleRight className="h-10 w-max" />
+        <button
+          onClick={handleOnNextFilmsClick}
+          className="cursor-pointer bg-white/20 h-max w-max py-6 rounded-xs hover:bg-white/30 duration-75 hover:scale-105 ml-10"
+        >
+          {onFirstSet ? (
+            <TfiAngleRight className="h-10 w-max" />
+          ) : (
+            <TfiAngleLeft className="h-10 w-max" />
+          )}
         </button>
       </div>
     </div>
